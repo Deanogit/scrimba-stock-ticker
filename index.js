7f0b2fb56392a37c5adaa92edf73b3f08ea25278
Dean Howe <hideanhowe@gmail.com>
2026-02-03 05:56:04 +0000

    index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+import { getStockData } from './fakeStockAPI';
+
+setInterval(function () {
+  const stockData = getStockData();
+  renderStockTicker(stockData);
+}, 1500);
+
+let prevPrice = null;
+
+function renderStockTicker(stockData) {
+  const stockDisplayName = document.getElementById('name');
+  const stockDisplaySymbol = document.getElementById('symbol');
+  const stockDisplayPrice = document.getElementById('price');
+  const stockDisplayPriceIcon = document.getElementById('price-icon');
+  const stockDisplayTime = document.getElementById('time');
+
+  const { name, sym, price, time } = stockData;
+
+  const priceDirectionIcon =
+    price > prevPrice
+      ? 'green.svg'
+      : price < prevPrice
+        ? 'red.svg'
+        : 'gray.svg';
+
+  const priceIconElement = document.createElement('img');
+  priceIconElement.src = `svg/${priceDirectionIcon}`;
+  priceIconElement.alt = 'Price direction icon';
+  stockDisplayPriceIcon.innerHTML = '';
+  stockDisplayPriceIcon.appendChild(priceIconElement);
+
+  stockDisplayName.innerHTML = name;
+  stockDisplaySymbol.innerHTML = sym;
+  stockDisplayPrice.innerHTML = price;
+  stockDisplayTime.innerHTML = time;
+
+  prevPrice = price;
+}
